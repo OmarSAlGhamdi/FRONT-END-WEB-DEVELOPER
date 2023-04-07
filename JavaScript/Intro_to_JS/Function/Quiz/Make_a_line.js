a62c7efb9d441d227d3a99bd089abab6b425d99e
Omar S AlGhamdi <goncr@hotmail.com>
2023-04-07 18:38:49 +0300

    make a line file

--- JavaScript/Intro_to_JS/Function/Quiz/Make_a_line.js
+++ JavaScript/Intro_to_JS/Function/Quiz/Make_a_line.js
@@ -1,10 +1,74 @@
-function makeLine(length) {
+// function makeline(lengthLines) {
+//     var line = "";
+
+//     for (var x = 1; x <= lengthLines; x++) {
+//         line += "* ";
+//     }
+
+//     return line  + "\n";
+// }
+
+// function buildTriangle(lineNumbers){
+//     var triangle = "";
+//     var lineNumberLoop = "";
+
+//     for(lineNumberLoop = 1; lineNumberLoop <= lineNumbers; lineNumberLoop++){
+//         triangle += makeline(lineNumberLoop);
+//     }
+
+//     return triangle;
+// }
+
+// console.log(buildTriangle(10));
+
+
+
+
+
+
+// function makeline(lineLength){
+//     var line = "";
+//     for(var x = 1; x <= lineLength; x++){
+//         line += "* ";
+//     }
+
+//     return line + "\n";
+// }
+
+// function buildTriangle(lineNumbers) {
+//     var triangle = "";
+//     var lineNumberLoop = "";
+
+//     for(lineNumberLoop = 1; lineNumberLoop <= lineNumbers; lineNumberLoop++){
+//         triangle += makeline(lineNumberLoop);
+//     }
+
+//     return triangle;
+// }
+
+// console.log(buildTriangle(3));
+
+
+// var lineLength = "";
+
+function makeLine(lineLength) {
+
     var line = "";
-    for (var j = 1; j <= length; j++) {
-        line +="* "
+    for (var x = 1; x <= lineLength; x++) {
+        line += "* ";
     }
-    return line + "/n";
+
+    return line + "\n";
 }
 
+function buildTriangle(lineHeight) {
+    var triangle = "";
+    var lineNumberLoop = "";
 
-makeLine(10);
+    for (lineNumberLoop = 1; lineNumberLoop <= lineHeight; lineNumberLoop++) {
+        triangle += makeLine(lineNumberLoop);
+    }
+    return triangle;
+}
+
+console.log(buildTriangle(10));
