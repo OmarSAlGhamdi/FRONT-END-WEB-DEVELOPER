26abbdcd4f69e431d90260aad753243ed9d16cc3
Omar S AlGhamdi <goncr@hotmail.com>
2023-06-17 03:33:00 +0300

    Adding Spread_Operator and Rest_Parameter

---
+++ JavaScript/Nano_Degree_On_Udacity/My_experimental_folder/Deconstructing.js
@@ -0,0 +1,11 @@
+const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
+
+
+const [ one ,,, two ,,, three] = things ;
+
+const colors = `list of colors
+${one}
+${two}
+${three}`;
+
+console.log(colors);
