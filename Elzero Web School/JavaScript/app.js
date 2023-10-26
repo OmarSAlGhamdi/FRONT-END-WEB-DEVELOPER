a57b74d77fc1e8a242a40aeced133ba27419bfc4
Omar S AlGhamdi <goncr@hotmail.com>
2023-10-27 01:54:05 +0300

    Adding new files in Elzero Folder

--- Elzero Web School/JavaScript/app.js
+++ Elzero Web School/JavaScript/app.js
@@ -1,8 +1,10 @@
-let h1Color = document.querySelectorAll('h1');
+/* let h1Color = document.querySelectorAll('h1');
 
 
 for(let i = 0; i < 1; i++){
     h1Color[i].style.color = 'red';
-}
+} */
 // h1Color.style.color = "red";
 
+
+console.log('Im Omar');
