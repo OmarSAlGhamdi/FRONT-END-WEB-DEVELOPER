9fcedfab61729508077216c1dce08517944d8fa7
Omar S AlGhamdi <goncr@hotmail.com>
2024-03-11 23:09:09 +0300

    Add new items on Elzero Rest parameter

--- Elzero Web School/JavaScript/Rest-Parameter..js
+++ Elzero Web School/JavaScript/Rest-Parameter..js
@@ -7,4 +7,13 @@ Function Advanced Practice :
 - Condition
 */
 
-function ShowInfo(user, age, rate, ...skills) {}
+function ShowInfo(user = "Unknown", age = "Unknown", rate = 0, ...skills) {
+  document.write(`<div>`);
+  document.write(`<h2>Welcome, ${user} 😘</h2>`);
+  document.write(`<p>Age: ${age} </p>`);
+  document.write(`<p>Hour Rate: ${rate}</p>`);
+  document.write(`<p>Skills : ${skills} </p>`);
+  document.write(`</div>`);
+}
+
+ShowInfo("Omar", 30, 100, "HTML" , "CSS");
