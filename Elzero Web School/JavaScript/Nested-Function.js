e3179a6f0c37651bb58c8b1e5e65c89ad35984bf
Omar S AlGhamdi <goncr@hotmail.com>
2024-03-16 02:13:09 +0300

    Add other examples to nested function

--- Elzero Web School/JavaScript/Nested-Function.js
+++ Elzero Web School/JavaScript/Nested-Function.js
@@ -1,6 +1,20 @@
 /* Example 1 */
 
-function sayMessage(firstName, lastName) {
+/* function sayMessage(firstName, lastName) {
+  let message = "Hello";
+  //   Nested Function
+  function concatMsg() {
+    return `${message} ${firstName} ${lastName}`;
+  }
+  concatMsg();
+  return message;
+}
+
+document.write(sayMessage("Omar", "AlGhamdi")); */
+
+/* Example 2 */
+
+/* function sayMessage(firstName, lastName) {
   let message = "Hello";
   //   Nested Function
   function concatMsg() {
@@ -9,4 +23,24 @@ function sayMessage(firstName, lastName) {
   return concatMsg();
 }
 
-document.write(sayMessage("Omar","AlGhamdi"));
+document.write(sayMessage("Omar", "AlGhamdi")); */
+
+            /* Example 3 */
+
+function sayMessage(firstName, lastName) {
+  let message = "Hello";
+  //   Nested Function
+  function concatMsg() {
+
+    function getFullName() {
+
+      return `${firstName} ${lastName}`;
+    }
+    //!when you call a function don't forgot to call them 
+    // with their parentheses ()
+    return `${message} ${getFullName()}`;
+  }
+  return concatMsg();
+}
+
+document.write(sayMessage("Omar", "AlGhamdi"));
