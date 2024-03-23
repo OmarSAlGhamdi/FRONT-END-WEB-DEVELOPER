368b1e47c8402f0e63838a5f68e323d1fbd73f91
Omar S AlGhamdi <goncr@hotmail.com>
2024-03-23 07:39:26 +0300

    Adding the high order Function files

---
+++ Elzero Web School/JavaScript/High-Order-Function(Map).js
@@ -0,0 +1,28 @@
+/* 
+Higher order Functions : 
+---> is a function that accept s functions as parameters and/or returns a function.
+
+-   Map
+--- method creates a new array.
+--- populated with the results of calling a provided function on every element
+--- in the calling array.
+
+Syntax map (callBackFunction(Element, Index, Array) { }, thisArg)
+-- Element => The current element being processed in the array.
+-- Index => The index of the current element being processed in the array.
+-- Array => The current Array.
+
+Notes 
+-   Map Return A New Array
+
+Examples :
+-- Anonymous Function
+-- Names Function
+
+*/
+
+let myNums = [1, 2, 3, 4, 5, 6];
+
+let newArray = [];
+
+
