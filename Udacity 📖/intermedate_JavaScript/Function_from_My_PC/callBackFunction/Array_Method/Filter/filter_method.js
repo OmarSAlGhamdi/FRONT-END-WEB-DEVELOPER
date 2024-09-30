238f2fbb4f90f2cbc17a0315a4f8e08d8e987c52
Omar S AlGhamdi <goncr@hotmail.com>
2024-09-30 16:47:26 +0300

    add filter folder

--- Udacity 📖/intermedate_JavaScript/Function_from_My_PC/callBackFunction/Array_Method/Filter/filter_method.js
+++ Udacity 📖/intermedate_JavaScript/Function_from_My_PC/callBackFunction/Array_Method/Filter/filter_method.js
@@ -1,1 +1,14 @@
+// Filter syntax : 
 
+
+
+
+// example of the filter : 
+
+const names = ['David' , 'Richard', 'Veronika'];
+
+const shortNames = names.filter(function(name){
+    return name.length < 6;
+});
+
+console.log(shortNames);
