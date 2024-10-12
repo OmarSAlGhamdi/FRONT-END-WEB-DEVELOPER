8c8a1bdc20769d9994b750aa56fbe2ed8bede6cf
Omar S AlGhamdi <goncr@hotmail.com>
2024-10-12 16:24:26 +0300

    adding example in constructor_function folder

--- Udacity 📖/intermedate_JavaScript/class_and_object/constructor_function/constructor_function.js
+++ Udacity 📖/intermedate_JavaScript/class_and_object/constructor_function/constructor_function.js
@@ -1,12 +1,53 @@
-function SoftwareDeveloper() {
+//Second Example Video On Udacity :
+function Hero(name , role){
+  // We use this.name and this.role to make instance for each new object
+  // (this.name) is the Refers to the property of the object that is being created by the constructor.
+  //(name) (on the right):Refers to the parameter passed into the function.
+  this.name = name; 
+  // console.log(name);
+  
+  this.role = role;
+
+  this.introduce = function(){
+    //this refer to this object that i want to call 
+    console.log(`My name is ${this.name} and my role is ${this.role}.`);
+    
+  }
+}
+
+let tylor = new Hero('Tylor' , 'Mother');
+
+let omar = new Hero('Omar' , 'CEO');
+
+// ✅ This can call directly because of the (console.log) inside the function:
+tylor.introduce(); 
+omar.introduce();
+
+// ❌ This can't call directly because it didn't have (console.log) inside it:
+console.log(omar.name);
+console.log(omar.role);
+
+
+
+//Second Example Video On Udacity :
+
+
+/* -------------------------------------------------- */
+
+
+//First Example Video On Udacity :
+
+/* function SoftwareDeveloper() {
   this.favoritLangauge = 'JavaScript';
 }
 
 let developer = new SoftwareDeveloper();
 
-developer()
+developer() */
+
+//First Example Video On Udacity :
 
 
+/* -------------------------------------------------- */
 
-
-// 
+//  : تجربتي في تطبيق الدرس 
