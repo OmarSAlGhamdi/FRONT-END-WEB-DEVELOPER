282b8f5c8de11e1438bef0e68a56f434a69e8441
Omar S AlGhamdi <goncr@hotmail.com>
2024-10-20 00:00:53 +0300

    adding content in Prototypal_Inheritance

--- Udacity 📖/intermedate_JavaScript/class_and_object/Prototypal_Inheritance/Prototypal_Inheritance.js
+++ Udacity 📖/intermedate_JavaScript/class_and_object/Prototypal_Inheritance/Prototypal_Inheritance.js
@@ -1,19 +1,48 @@
 /*  ⬇️ ----------------------- First Example On Udacity GPT ----------------------- ⬇️ : */
+// * The Dog function is a constructor.
 
-function Dog(name) {
-    this.name = name; // This property is unique to each instance
-  }
-  
-  // Adding a method to the prototype
-  Dog.prototype.bark = function() {
-    console.log(`${this.name} says woof!`);
-  };
-  
-  // Creating a new instance of Dog
-  const myDog = new Dog('Buddy');
-  
-  // Accessing the property and method
-  console.log(myDog.name); // Outputs: Buddy
-  myDog.bark(); // Outputs: Buddy says woof!
+/* function Dog(name) {
+  this.name = name; // This property is unique to each instance
+} */
 
-  /* ⬆️ ----------------------- First Example On Udacity GPT ----------------------- ⬆️ */
+// Adding a method to the prototype
+/* Dog.prototype.bark = function () {
+ */ /* The (bark method) is added to the Dog.prototype,
+     which means all instances of Dog can access this method. */
+
+/*   console.log(`${this.name} says woof!`);
+}; */
+
+// Creating a new instance of Dog
+/* const myDog = new Dog("Buddy"); */
+
+/* When myDog is created, it has its own name property, 
+but it can also use the bark method defined on the Dog.prototype. */
+
+/* 
+// Accessing the property and method
+console.log(myDog.name); // Outputs: Buddy
+myDog.bark(); // Outputs: Buddy says woof!
+
+ */
+/* ⬆️ ----------------------- First Example On Udacity GPT ----------------------- ⬆️ */
+
+/* ⬇️ ----------------------- Second Example On Udacity ----------------------- ⬇️ */
+
+function Dog(name, age, weight) {
+  this.name = name;
+  this.age = age;
+  this.weight = weight;
+}
+
+Dog.prototype.bark = function () {
+  console.log(
+    `His name is ${this.name}, his age is ${this.age},His weight is ${this.weight}.`
+  );
+};
+
+const myDog = new Dog("Haskey", 20, 100);
+
+myDog.bark();
+
+/* ⬆️ ----------------------- Second Example On Udacity ----------------------- ⬆️ */
