e0d29f1717228bcab30082fe8046854c19e9247d
Omar S AlGhamdi <goncr@hotmail.com>
2024-11-14 02:50:01 +0300

    Orgnizing folders

--- Depricated_Examples.js
+++ Depricated_Examples.js
@@ -1,33 +1,8 @@
 // ⬇️ ----------------------- First Example  On Udacity ----------------------- ⬇️ :
 
-/* function Cat(name) {
-  this.name = name;
-  this.sayName = function () {
-    console.log(`Meow my name is ${this.name}`);
-  };
-}
-
-const baily = new Cat("Baily");
-
-baily.sayName(); */
-
 // ⬆️ ----------------------- First Example  On Udacity ----------------------- ⬆️ :
 
 // ⬇️ ----------------------- Second Example On Udacity ----------------------- ⬇️ :
-
-const dog = {
-  bark: function () {
-    console.log("Woof!");
-  },
-  barkTwice: function () {
-    this.bark();
-    this.bark();
-  },
-};
-
-dog.bark();
-
-dog.barkTwice();
 
 // ⬆️ ----------------------- Second Example On Udacity ----------------------- ⬆️ :
 
---
+++ Udacity/intermedate_JavaScript/class_and_object/this/this.js
@@ -0,0 +1,81 @@
+console.log(
+  "⬇️  ---------------------✅ First Example Start ✅--------------------- ⬇️"
+);
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+
+/* ⬇️ ----------------------- 1 First Example On Udacity 1 ----------------------- ⬇️ */
+
+function Cat(name) {
+  this.name = name;
+  this.sayName = function () {
+    console.log(`Meow my name is ${this.name}`);
+  };
+}
+
+const baily = new Cat("Baily");
+
+baily.sayName();
+
+/* ⬆️ ----------------------- 1 First Example On Udacity 1 ----------------------- ⬆️ */
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+console.log(
+  "⬆️  ---------------------❌ First Example Finish ❌--------------------- ⬆️"
+);
+console.log(
+  `
+
+`
+);
+
+console.log(
+  "⬇️  ---------------------✅ Second Example Start ✅--------------------- ⬇️"
+);
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+/* ⬇️ ----------------------- 2 Second Example On Udacity 2 ----------------------- ⬇️ */
+
+const dog = {
+  bark: function () {
+    console.log("Woof!");
+  },
+  barkTwice: function () {
+    this.bark();
+    this.bark();
+  },
+};
+
+dog.bark();
+
+dog.barkTwice();
+
+/* ⬆️ ----------------------- 2 Second Example On Udacity 2 ----------------------- ⬆️ */
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+console.log(
+  "⬆️  ---------------------❌ Second Example Finish ❌--------------------- ⬆️"
+);
+console.log(
+  `
+
+    `
+);
+
+
+
+
+
+
+console.log(
+  "⬇️  ---------------------✅ 🛠️💡My Experiment Example Start💡🛠️ ✅--------------------- ⬇️"
+);
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+/* ⬇️ ----------------------- 🛠️💡My Experiment Example💡🛠️ ----------------------- ⬇️ */
+
+/* ⬆️ ----------------------- 🛠️💡My Experiment Example💡🛠️ ----------------------- ⬆️ */
+console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+console.log(
+  "⬆️  ---------------------❌ 🛠️💡My Experiment Example Finish💡🛠️ ❌--------------------- ⬆️"
+);
+console.log(
+  `
+  
+    `
+);
