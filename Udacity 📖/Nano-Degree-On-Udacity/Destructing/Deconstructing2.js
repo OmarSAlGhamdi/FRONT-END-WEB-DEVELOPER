270f18a9aa64e714f5d295abd3bfcf3f7059af71
Omar S AlGhamdi <goncr@hotmail.com>
2024-11-25 17:37:53 +0300

    update some files

--- Udacity 📖/Nano-Degree-On-Udacity/Destructing/Deconstructing2.js
+++ Udacity 📖/Nano-Degree-On-Udacity/Destructing/Deconstructing2.js
@@ -1,7 +1,8 @@
 let numbers = [1, 25, 10, 20 , 30];
 // The index should be written their before calling them on console.log() or 
 // You'll face a bug because you'll call unidentfied/unknown items !!
-let [ a = 10, b =20 , c = 30] = numbers; //⬅︎ we mention the variable name at the end if we use this way
+let [ a = 10, b =20 , c = 30] = numbers; //⬅︎ we mention the variable name at the end 
+                                         // if we use this way
 
 
 // You can call any items you want only if it's mentioned before ⬆︎
--- Udacity 📖/Nano-Degree-On-Udacity/Destructing/Destructing.js
+++ Udacity 📖/Nano-Degree-On-Udacity/Destructing/Destructing.js
@@ -1,6 +1,69 @@
-const point = [10, 25, -34];
 
-const [x, y, z ] = point;
 
-console.log(x, y);
 
+
+
+console.log(
+    "⬇️  ---------------------✅ 1 First Example Start 1 ✅--------------------- ⬇️"
+  );
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  /* ⬇️ ----------------------- 1 First Example On Udacity 1 ----------------------- ⬇️ */
+  
+  const point = [10, 25, -34];
+
+  const [x, y, z ] = point;
+  
+  console.log(x, y);
+
+  /* ⬆️ ----------------------- 1 First Example On Udacity 1 ----------------------- ⬆️ */
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  console.log(
+    "⬆️  ---------------------❌ 1 First Example Finish 1 ❌--------------------- ⬆️"
+  );
+  console.log(
+    `
+  
+  `
+  );
+  
+  console.log(
+    "⬇️  ---------------------✅ 2 Second Example Start 2 ✅--------------------- ⬇️"
+  );
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  /* ⬇️ ----------------------- 2 Second Example On Udacity 2 ----------------------- ⬇️ */
+  
+
+  const numbers = [1, 2, 3, 4, 5];
+  const [first, second, ...rest] = numbers;
+  console.log(first); // 1
+  console.log(second); // 2
+  console.log(rest); // [3, 4, 5]
+
+
+  /* ⬆️ ----------------------- 2 Second Example On Udacity 2 ----------------------- ⬆️ */
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  console.log(
+    "⬆️  ---------------------❌ 2 Second Example Finish 2 ❌--------------------- ⬆️"
+  );
+  console.log(
+    `
+  
+      `
+  );
+  
+  console.log(
+    "⬇️  ---------------------✅ 3 Third Example Start 3 ✅--------------------- ⬇️"
+  );
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  /* ⬇️ ----------------------- 3 Third Example On Udacity 3 ----------------------- ⬇️ */
+  
+  /* ⬆️ ----------------------- 3 Third Example On Udacity 3 ----------------------- ⬆️ */
+  console.log(``); // عشان يكون في مسافة قبل كلمة المثال
+  console.log(
+    "⬆️  ---------------------❌ 3 Third Example Finish 3 ❌--------------------- ⬆️"
+  );
+  console.log(
+    `
+    
+      `
+  );
