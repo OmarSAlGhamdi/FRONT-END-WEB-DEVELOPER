428f1a128e7b8876a7e8d683c3df1ee9bf11b23d
Omar S AlGhamdi <31236422+OmarSAlGhamdi@users.noreply.github.com>
2024-12-22 00:20:35 +0300

    add content to Elzero folder

---
+++ Elzero Web School/JavaScript/Regular Expression/Regular Expression – Form Validation/Regular Expression – Form Validation.js
@@ -0,0 +1,10 @@
+/*
+  Regular Expression
+  - Input Form Validation Practice
+*/
+
+document.getElementById("register").onsubmit = function () {
+  let phoneInput = document.getElementById("phone").value;
+  let phneRe = /\(\d{4}\)\s\d{3}-\d{4}/; // (1234) 567-8910
+  let ValidationResult = 
+};
